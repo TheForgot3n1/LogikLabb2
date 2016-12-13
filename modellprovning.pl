91633ac1f44a6d228d57d13dbc17c3543fbea9b9
TheForgot3n1 <gerdano_jetmotor@hotmail.com>
2016-12-13 11:11:46 +0100

    Added AX, EX and started on AG

--- modellprovning.pl
+++ modellprovning.pl
@@ -18,8 +18,6 @@ check(T, L, S, [], F).
 % (Läses F är sann i tillståndet S med tanke på slingdetektionen U)
 % To execute: consult('your_file.pl'). verify('input.txt').
 % Literals
-% check_if_right_state(S,[H|T]) :-
-%  (member(S, H); check_if_right_state(S,T))
 
 check(_,[],_,_,_).
 
@@ -30,15 +28,13 @@ check_ext1([H|R], X) :-
   member(X,T).
 
 check(_, [H|R], S, [], neg(X)) :-
-  ((member(S,H), check_ext1(H, X)), check_ext1(H, X)); check(_, R, S, [], neg(X))),
-  check_ext2(_, H, S, [], X).
+  ((member(S,H), check_ext1(H, X)), check_ext2(H, X)); check(_, R, S, [], neg(X))).
 check_ext2(_, [H|R], S, [], X) :-
   flatten(R,T),
   \+ member(X,T).
 
 check(T, [H|R], S, [], and(F,G)) :-
-  ((member(S,H), check_ext1(H, X)); check(T, R, S, [], and(F,G))),
-  check_ext3(T, H, [], and(F,G)).
+  ((member(S,H), check_ext3(H, X)); check(T, R, S, [], and(F,G))).
 check_ext3(T, [H|R], [], and(F,G)) :-
   flatten(R,T),
   member(F,T),
@@ -54,25 +50,37 @@ check_ext4(_, [H|R], [], and(F,G)) :-
 % AX
 % enklaste: s1 -> s2 -> s3 -> (ingen)
 %           s1 -> s4 -> s5 -> (ingen)
-check(Adj, [H|T], S, [], X) :-
-  findAdjacents(S,Adj,Targets),
-  check_AXHelper(Adj,[H|T],S,U,X,Targets).
+check_AX([],_,_,_,_)
 
-check_AXHelper(Adj, L, S, U, X, Targets) :-
-  \+ member(S,U),
-  check(_,L,S, [], X),
-  append(S,U,UAppend),
-  check(Adj,)
-  check(Adj, [H|T], Next, UAppend, X).
-  check_AXHelper(Adj, [H|T], S, UAppend, X) :-
+check_AX([H1|T1], [H2|T2], S, _, AX(F))
+  check_AXHelper(H1,H2,F),
+  check_AX(T1,[H2|T2],S,U,F).
 
-findAdjacents(S, [H1|T1], Targets) :-
-  (member(S,H1), flatten(T1,Flat), permutation(Flat, Targets));
-  findAdjacents(S,T1,Targets).
+check_AXHelper(AdjState,[H|T],F) :-
+  AdjState = H -> member(F,T),
+  check_AXHelper(AdjState,T,F).
 
 
 % EX
+check_EX([],_,_,_,_)
+
+check_EX([H1|T1], [H2|T2], S, _, EX(F))
+  (check_EXHelper(H1,H2,F); check_EX(T1,[H2|T2],S,U,F)).
+
+check_EXHelper(AdjState,[H|T],F) :-
+  AdjState = H -> member(F,T),
+  check_AXHelper(AdjState,T,F).
+
+
 % AG
+check_AG([H1|T1], [H2|T2], S, _, AG(F))
+  check_AGHelper(H1,H2,F),
+  check_AG(T1,[H2|T2],S,U,F).
+
+check_AGHelper(AdjState,[H|T],F) :-
+  AdjState = H -> member(F,T),
+  check_AGHelper(AdjState,T,F).
+
 % EG
 % EF
 % AF
