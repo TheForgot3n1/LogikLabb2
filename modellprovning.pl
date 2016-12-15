b228abeb8ff0ce3afb4d7798eb535536ca8674d8
Jonathan Broström <jonis555@yahoo.se>
2016-12-15 10:36:33 +0100

    Fixed check for everything up to EX

--- modellprovning.pl
+++ modellprovning.pl
@@ -23,43 +23,110 @@ find_and_get_list([H|L],AL,S) :-
   member(S,H) -> find_and_get_list(H,AL),
   find_and_get_list(L,AL,S).
 
-find_and_get_list([H|T],AL) :-
+find_and_get_list([_|T],AL) :-
   AL = T.
 
-check(_,[],_,_,_).
+check_ext([_|T], X) :-
+  flatten(T, R),
+  member(X, R).
 
-check(_, [H|R], S, [], X) :-
-  ((member(S,H), check_ext1(H, X)); check(_, R, S, [], X)), !.
-check_ext1([H|R], X) :-
+check(_,[],_,_,_) :- fail.
+
+check(_, [H|R], S, [], F) :-
+  ((member(S,H), check_ext(H, F)); check(_, R, S, [], F)), !.
+  /*
+check_ext1([_|R], X) :-
   flatten(R,T),
   member(X,T).
-
-check(_, [H|R], S, [], neg(X)) :-
-  ((member(S,H), check_ext1(H, X)), check_ext2(H, X)); check(_, R, S, [], neg(X))).
+*/
+check(_, [H|R], S, [], neg(F)) :-
+  (member(S,H), \+ check_ext(H, F));
+  check(_, R, S, [], neg(F)).
+  /*
 check_ext2(_, [H|R], S, [], X) :-
   flatten(R,T),
   \+ member(X,T).
-
+*/
 check(T, [H|R], S, [], and(F,G)) :-
-  ((member(S,H), check_ext3(H, X)); check(T, R, S, [], and(F,G))).
+  ((member(S,H), check_ext(H, F), check_ext(H, G));
+  check(T, R, S, [], and(F,G))).
+  /*
 check_ext3(T, [H|R], [], and(F,G)) :-
   flatten(R,T),
   member(F,T),
   member(G,T).
-
-check(_, L, S, [], or(F,G)) :-
-  ((member(S,H), check_ext1(H, X)); check(_, R, S, [], and(F,G))),
-  check_ext4(_, H, [], and(F,G)).
+*/
+check(_, [H|R], S, [], or(F,G)) :-
+  (member(S,H), (check_ext(H, F); check_ext(H, G)));
+  check(_, R, S, [], and(F,G)).
+  /*
 check_ext4(_, [H|R], [], and(F,G)) :-
   flatten(R,T),
   (member(F,T); member(G,T)).
-
+*/
 % AX
 % enklaste: s1 -> s2 -> s3 -> (ingen)
 %           s1 -> s4 -> s5 -> (ingen)
-check_AX([],_,_,_,_)
+check(T, L, S, _, ax(F)) :-
+  find_and_get_list(T, AL, S),
+  check_AX(AL, L, S, _, F).
 
-check_AX(Adj, [H2|T2], S, _, AX(F))
+check(T, L, S, _, ex(F)) :-
+  find_and_get_list(T, AL, S),
+  check_EX(AL, L, S, _, F).
+
+%check(T, L, S, _, af(F)) :-
+%  find_and_get_list(T, AL, S),
+%  fail.
+
+check_AX([],_,_,_,_).
+
+check_AX([H|T], L, _, _, F) :-
+  check(_, L, H, _, F),
+  check_AX(T, L, _, _, F).
+
+check_EX([],_,_,_,_) :-
+  fail.
+
+check_EX([H|T], L, _, _, F) :-
+  check(_, L, H, _, F);
+  check_EX(T, L, _, _, F).
+
+
+
+check(T, L, S, U, ag(F)) :-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+check_AX(Adj, [H2|T2], S, _, F) :-
   find_and_get_list(Adj,AL,S),
   check_AXHelper1(AL,[H2|T2], S, _, F).
 
@@ -73,14 +140,14 @@ check_AXHelper2(AdjState,[H|T],F) :-
 
 
 % EX
-check_EX([],_,_,_,_)
+%check_EX([],_,_,_,_).
 
-check_EX(Adj, [H2|T2], S, _, EX(F)) :-
+check_EX(Adj, [H2|T2], S, _, F) :-
   find_and_get_list(Adj,AL,S),
   check_EXHelper1(AL,[H2|T2], S, _, F).
 
 check_EXHelper1([H1|T1],[H2|T2], S, _, F) :-
-  (check_EXHelper2(H1,H2,F); check_EXHelper1(T1,[H2|T2],S,U,F)).
+  (check_EXHelper2(H1,H2,F); check_EXHelper1(T1,[H2|T2],S,_,F)).
 
 check_EXHelper2(AdjState,[H|T],F) :-
   AdjState = H -> member(F,T),
@@ -88,14 +155,14 @@ check_EXHelper2(AdjState,[H|T],F) :-
 
 
 % AG
-check_AG(Adj, [H2|T2], S, U, AG(F))
+check_AG(Adj,[H2|T2],S,U,F) :-
   find_and_get_list(Adj,AL,S),
   check_all_routes(AL,[H2|T2],S,U,F),
-  check_AGHelper(AL, [H2|T2], S, U, AG(F))
+  check_AGHelper(AL,[H2|T2],S,U,AG(F)),
+  check_all_routes([H1|T1],[H2|T2],S,U,F),
+  check_AGHelper(AdjState,[H|T],F).
 
-check_all_routes([H1|T1],[H2|T2],S,U,F)
-check_AGHelper(AdjState,[H|T],F) :-
-
+check_AGHelper(_, _, _, _, _).
 % EG
 % EF
 % AF
@@ -104,4 +171,4 @@ check_AGHelper(AdjState,[H|T],F) :-
 % Adjacency lists of LTS
 % im = [[s0, [s1, s2]],[s1, [s0, s2]],[s2, [s2]]].
 % Labeling of LTS
-% lt = [[s0, [p,q]],[s1, [q,r]],[s2, [r]]].
+% lt = [[s0, [p,q]],[s1, [q,r]],[s2, [r]]].*/
