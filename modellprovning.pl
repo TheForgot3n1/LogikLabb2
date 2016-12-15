c71c8da52679b167dbcd12c0d86f2310d8ec7ebb
Rafael Dolfe <rafaeld@kth.se>
2016-12-15 11:13:28 +0100

    EF

--- modellprovning.pl
+++ modellprovning.pl
@@ -34,36 +34,19 @@ check(_,[],_,_,_) :- fail.
 
 check(_, [H|R], S, [], F) :-
   ((member(S,H), check_ext(H, F)); check(_, R, S, [], F)), !.
-  /*
-check_ext1([_|R], X) :-
-  flatten(R,T),
-  member(X,T).
-*/
+
 check(_, [H|R], S, [], neg(F)) :-
   (member(S,H), \+ check_ext(H, F));
   check(_, R, S, [], neg(F)).
-  /*
-check_ext2(_, [H|R], S, [], X) :-
-  flatten(R,T),
-  \+ member(X,T).
-*/
+
 check(T, [H|R], S, [], and(F,G)) :-
   ((member(S,H), check_ext(H, F), check_ext(H, G));
   check(T, R, S, [], and(F,G))).
-  /*
-check_ext3(T, [H|R], [], and(F,G)) :-
-  flatten(R,T),
-  member(F,T),
-  member(G,T).
-*/
+
 check(_, [H|R], S, [], or(F,G)) :-
   (member(S,H), (check_ext(H, F); check_ext(H, G)));
   check(_, R, S, [], and(F,G)).
-  /*
-check_ext4(_, [H|R], [], and(F,G)) :-
-  flatten(R,T),
-  (member(F,T); member(G,T)).
-*/
+
 % AX
 % enklaste: s1 -> s2 -> s3 -> (ingen)
 %           s1 -> s4 -> s5 -> (ingen)
@@ -121,74 +104,15 @@ check_exists(T, L, [H|T], U, F) :-
   check(T, L, H, U, F);
   check_exists(T, L, T, U, F).
 
+check_EF(_,_,_,_,[]) :-
+  fail.
 
+check_EF(T, L, U, F, [H|T]) :-
+  check_EFHelper(T,L,U,F,H); check_EF(T,L,U,F,T).
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-check_AX(Adj, [H2|T2], S, _, F) :-
-  find_and_get_list(Adj,AL,S),
-  check_AXHelper1(AL,[H2|T2], S, _, F).
-
-check_AXHelper1([H1|T1],[H2|T2], S, _, F).
-  check_AXHelper2(H1,H2,F),
-  check_AXHelper1(T1,[H2|T2],S,U,F).
-
-check_AXHelper2(AdjState,[H|T],F) :-
-  AdjState = H -> member(F,T),
-  check_AXHelper(AdjState,T,F).
-
-
-% EX
-%check_EX([],_,_,_,_).
-
-check_EX(Adj, [H2|T2], S, _, F) :-
-  find_and_get_list(Adj,AL,S),
-  check_EXHelper1(AL,[H2|T2], S, _, F).
-
-check_EXHelper1([H1|T1],[H2|T2], S, _, F) :-
-  (check_EXHelper2(H1,H2,F); check_EXHelper1(T1,[H2|T2],S,_,F)).
-
-check_EXHelper2(AdjState,[H|T],F) :-
-  AdjState = H -> member(F,T),
-  check_AXHelper(AdjState,T,F).
-
-
-% AG
-check_AG(Adj,[H2|T2],S,U,F) :-
-  find_and_get_list(Adj,AL,S),
-  check_all_routes(AL,[H2|T2],S,U,F),
-  check_AGHelper(AL,[H2|T2],S,U,AG(F)),
-  check_all_routes([H1|T1],[H2|T2],S,U,F),
-  check_AGHelper(AdjState,[H|T],F).
-
-check_AGHelper(_, _, _, _, _).
-% EG
-% EF
-% AF
-
-% States are s0, s1 and s2
-% Adjacency lists of LTS
-% im = [[s0, [s1, s2]],[s1, [s0, s2]],[s2, [s2]]].
-% Labeling of LTS
-% lt = [[s0, [p,q]],[s1, [q,r]],[s2, [r]]].*/
+check_EFHelper(T,L,U,F,H) :-
+  member(H,U) -> fail,
+  append(H,U,HU),
+  find_and_get_list(L,AL1,H),
+  %find_and_get_list(T,AL2,H),
+  (member(F,AL1);check_EF(T,L,H,U,F)).
